efc042b3137f0fd615fc3c5bc5d9a6eebdcbd3b8
Alessia9802 <alessiamisciagna@yahoo.it>
2021-11-15 16:00:06 +0100

    Props doesn't work properly

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,25 +1,45 @@
 <template>
-  <div class="container" v-for="card in cards" :key="card.poster"></div>
+  <div>
+    <div
+      class="container"
+      v-for="card in cards"
+      :key="card.poster"
+      :title="card.title"
+      :author="card.author"
+      :genre="card.genre"
+      :year="card.year"
+    >
+      {{ card.poster }}{{ card.title }}{{ card.author }}{{ card.genre }}
+      {{ card.year }}
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
+  props: {
+    key: String,
+    title: String,
+    author: String,
+    genre: String,
+    year: String,
+  },
   data() {
     return {
       cards: [],
     };
   },
-},
 
-mounted(){
-  axios
-    .get("https://flynn.boolean.careers/exercises/api/array/music")
-    .then((r) => {
-      this.cards = r.data;
-      console.log(r.data);
-    });
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((r) => {
+        this.cards = r.data;
+        console.log(r.data);
+      });
+  },
 };
 </script>
 
