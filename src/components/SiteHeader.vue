5cca7a359e072799f7ae055bf063792a2b6e6833
Alessia9802 <alessiamisciagna@yahoo.it>
2021-11-16 13:07:27 +0100

    Add select option genre

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -5,6 +5,15 @@
       src="https://www.ore25.it/wp-content/uploads/2020/01/file-spotify-logo-png-4.png"
       alt=""
     />
+    <div class="selection">
+      <label class="select_genre" for="dischi">Seleziona un genere</label>
+      <select class="generi" name="genere" id="genere">
+        <option value="Rock">Rock</option>
+        <option value="Pop">Pop</option>
+        <option value="Jazz">Jazz</option>
+        <option value="Metal">Metal</option>
+      </select>
+    </div>
   </nav>
 </template>
 
@@ -16,9 +25,21 @@ export default {};
 nav {
   height: 70px;
   background-color: #2e3a46;
+  display: flex;
+  align-items: center;
 }
 .logo_spotify {
   width: 50px;
   margin: 10px;
+}
+.select_genre {
+  color: white;
+  padding: 0.5rem;
+}
+.generi {
+  width: 100px;
+  padding: 0.1rem;
+  border: 3px solid rgb(119, 119, 168);
+  border-radius: 40px;
 }
 </style>
